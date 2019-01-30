327e3272ea8f647beded1a03701f7c6b2f139e82
TonyDMorris <anthonymorris86@googlemail.com>
2019-01-30 21:12:07 +0000

    got the display working and i can add digits to it.

---
+++ main.js
@@ -0,0 +1,9 @@
+let inputValueOne = []
+let inputValueTwo = []
+let signValue = "";
+
+
+function addDigits(digit){
+inputValueOne.push(digit)
+document.getElementById("display").innerText = inputValueOne.join("");
+}
