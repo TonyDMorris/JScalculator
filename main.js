c98fc9d353d8278ce139fd6fa824539bfa1c0adc
TonyDMorris <anthonymorris86@googlemail.com>
2019-01-30 21:26:11 +0000

    working on getting the signHandler to work

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-let inputValueOne = []
-let inputValueTwo = []
-let signValue = "";
+let inputValueOne = [];
+let inputValueTwo = [];
+let signValue;
 
 
 function addDigits(digit){
 inputValueOne.push(digit)
 document.getElementById("display").innerText = inputValueOne.join("");
+}
+
+function signHandler(sign){
+    if(!signValue){
+    signValue = sign;
+    inputValueTwo = inputValueOne;
+    inputValueOne = [0];
+    document.getElementById("display").innerText = inputValueOne.join("");
+    }
+    else{
+        signValue = sign;  
+    }
+}
+
+function equals(){
+    switch
 }
